1be5049207f435c93b5b0d1b66a67d868895786a
LETICIA EVELYN DE SOUZA LUBRIGATE <leticia.eslubrigate@senacsp.edu.br>
2023-10-11 14:16:09 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function menu(){
+
+    let menu = document.getElementById("menu-area")
+
+    if(menu.style.width == '200px'){
+        menu.style.width = '0px'
+    } else{
+        menu.style.width = '200px'
+    }
+}
